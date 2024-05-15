03c230668a916f7d73c590ecb33791de83848907
Matt Ondo <yours.truly@mattondo.com>
2024-05-15 17:44:39 -0400

    initial commit

---
+++ src/components/results.tsx
@@ -0,0 +1,56 @@
+import { Modal, IconButton, IconNavigateBack32, IconCross32, Text, Button } from '@create-figma-plugin/ui'
+import { h } from 'preact'
+import { useEffect, useState } from 'preact/hooks'
+
+function ResultsModal({handleCopy, handleExit, output, textAreaRef}: {handleCopy: any, handleExit: any, output: string, textAreaRef: any}) {
+
+  const [isOpen, setIsOpen] = useState(false)
+
+  function handleCloseButtonClick(event: any) {
+    setIsOpen(false);
+  }
+
+  useEffect(() => {
+    window.addEventListener('message', (message) => {
+      if (message.data.pluginMessage.type === 'generated') {
+        const data = JSON.parse(message.data.pluginMessage.data);
+        setIsOpen(true)
+      } else if (message.data.pluginMessage.type === 'error') {
+        console.error('Error', message.data.pluginMessage.data)
+        setIsOpen(false);
+      }
+    })
+  }, []);
+
+  return (
+    <Modal open={isOpen} transition={true} position="right">
+      <div style={{ width: '100%', height: '100%' }} class="flex flex-col gap-2 p-4">
+        <div class="flex gap-4 justify-between">
+          <IconButton onClick={handleCloseButtonClick} title="Go back">
+            <IconNavigateBack32 />
+          </IconButton>
+          <Button onClick={handleCopy} class="w-full">
+            <Text>Copy to Clipboard</Text>
+          </Button>
+          <IconButton onClick={handleExit} title="Close">
+            <IconCross32 />
+          </IconButton>
+        </div>
+        <div class="font-mono h-full">
+          <textarea 
+            name="output" 
+            class="overflow-x-scroll overflow-wrap whitespace-pre w-full h-full resize-none bg-transparent"
+            ref={textAreaRef} 
+            value={output} 
+            onClick={handleCopy} 
+            readOnly
+            >
+              Select all
+          </textarea>
+        </div>
+      </div>
+    </Modal>
+  )
+}
+
+export { ResultsModal }
---
+++ src/ui.tsx
@@ -0,0 +1,64 @@
+import { render, VerticalSpace } from '@create-figma-plugin/ui'
+import { emit } from '@create-figma-plugin/utilities'
+import { Form } from './components/form'
+import { ResultsModal } from './components/results'
+import { h } from 'preact'
+import { useEffect, useState, useRef } from 'preact/hooks'
+import '!./styles/output.css'
+
+function Plugin () {
+  const [loading, setLoading] = useState(false)
+  const [output, setOutput] = useState('')
+  const textAreaRef = useRef(null)
+
+  function handleExit() {
+    emit('close')
+  }
+
+  useEffect(() => {
+    window.addEventListener('message', (message) => {
+      if (message.data.pluginMessage.type === 'generated') {
+        const data = JSON.parse(message.data.pluginMessage.data);
+        setOutput(data)
+        setLoading(false)
+      } else if (message.data.pluginMessage.type === 'error') {
+        console.error('Error', message.data.pluginMessage.data)
+        setLoading(false)
+        setOutput('')
+      }
+    })
+  }, []);
+  
+  function handleCopy(e:any) {
+    e.preventDefault();
+    emit('copy', {output});
+
+    (textAreaRef.current! as HTMLTextAreaElement).select()
+    document.execCommand('copy')
+
+  }
+
+  return (
+    <div className="flex flex-col px-4 h-full" >
+      <VerticalSpace space="medium" />
+
+      <Form 
+        loading={loading} 
+        setLoading={setLoading} 
+        setOutput={setOutput} 
+        handleExit={handleExit} 
+      />
+
+      <ResultsModal
+        handleCopy={handleCopy}
+        handleExit={handleExit}
+        output={output}
+        textAreaRef={textAreaRef}
+      />
+
+      <VerticalSpace space="medium" />
+    </div>
+  )
+}
+
+export default render(Plugin)
